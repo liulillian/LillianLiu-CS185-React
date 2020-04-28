52995c9489a30010e3a95ad7f339b74b158edaf6
liulillian <l_liu@umail.ucsb.edu>
2020-04-27 19:59:03 -0700

    Updates

---
+++ precache-manifest.2e42cd421d1e6543d591041a9c26ce1d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7803b7689930d1839560a8abed0b1c43",
+    "url": "/LillianLiu-CS185-React/index.html"
+  },
+  {
+    "revision": "15af3b28df905de13d93",
+    "url": "/LillianLiu-CS185-React/static/css/main.b53785af.chunk.css"
+  },
+  {
+    "revision": "957308e8faaf5fc74258",
+    "url": "/LillianLiu-CS185-React/static/js/2.8144bc12.chunk.js"
+  },
+  {
+    "revision": "04007fcd50e895bcbb2b21327be92c6e",
+    "url": "/LillianLiu-CS185-React/static/js/2.8144bc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15af3b28df905de13d93",
+    "url": "/LillianLiu-CS185-React/static/js/main.464d7bde.chunk.js"
+  },
+  {
+    "revision": "586e600648c783edf91a",
+    "url": "/LillianLiu-CS185-React/static/js/runtime-main.cc53a3f9.js"
+  },
+  {
+    "revision": "6dcc18302cf3f34933dcc1d0de961fd3",
+    "url": "/LillianLiu-CS185-React/static/media/BG-tealfloral.6dcc1830.png"
+  },
+  {
+    "revision": "9d0630db0db028705e1ed39e899b9327",
+    "url": "/LillianLiu-CS185-React/static/media/BG_tp.9d0630db.jpg"
+  },
+  {
+    "revision": "4dcd2f242fb589115ce2ceab2a4062b1",
+    "url": "/LillianLiu-CS185-React/static/media/art1.4dcd2f24.png"
+  },
+  {
+    "revision": "2da86e312a8046f0ae98b38be73087cf",
+    "url": "/LillianLiu-CS185-React/static/media/art2.2da86e31.png"
+  },
+  {
+    "revision": "7ca26466356d6aeb8ec36b04b85270e1",
+    "url": "/LillianLiu-CS185-React/static/media/art3.7ca26466.png"
+  },
+  {
+    "revision": "573587b385bf1f50c374bf35f45bcdd5",
+    "url": "/LillianLiu-CS185-React/static/media/art4.573587b3.gif"
+  },
+  {
+    "revision": "68bd48e79216c4c50ea4a9980ca24a29",
+    "url": "/LillianLiu-CS185-React/static/media/photo0.68bd48e7.jpg"
+  },
+  {
+    "revision": "836cdb71e1ece0e558ca241fe547cdb8",
+    "url": "/LillianLiu-CS185-React/static/media/photo1.836cdb71.jpg"
+  },
+  {
+    "revision": "3334f86cfaf84ab85d2f665a9652943f",
+    "url": "/LillianLiu-CS185-React/static/media/photo2.3334f86c.jpg"
+  },
+  {
+    "revision": "b44de0e87b386e1818a74a1697707dd6",
+    "url": "/LillianLiu-CS185-React/static/media/photo3.b44de0e8.jpg"
+  },
+  {
+    "revision": "f4b0139cea7c9a373d7724b89fbeb627",
+    "url": "/LillianLiu-CS185-React/static/media/photo4.f4b0139c.jpg"
+  },
+  {
+    "revision": "be1f2b1af6eff36913c03bd689ab7a1c",
+    "url": "/LillianLiu-CS185-React/static/media/photo5.be1f2b1a.jpg"
+  },
+  {
+    "revision": "598f2449d46601312743ce34f17cb20d",
+    "url": "/LillianLiu-CS185-React/static/media/photo6.598f2449.jpg"
+  },
+  {
+    "revision": "fa3b1ecae98980745c352e5c1bb8c559",
+    "url": "/LillianLiu-CS185-React/static/media/photo7.fa3b1eca.jpg"
+  }
+]);
