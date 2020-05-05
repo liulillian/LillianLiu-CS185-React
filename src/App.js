02eadd9faa25fdf6d5583b8e2651351e9b401fed
liulillian <l_liu@umail.ucsb.edu>
2020-05-04 19:04:22 -0700

    Basic firebase setup for GuestBook.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //App.js
-import React, {Component, useState, useEffect} from 'react';
+import React, {Component, useState} from 'react';
 import SimpleReactLightbox from 'simple-react-lightbox';
 import ScrollUpButton from 'react-scroll-up-button';
 import './App.css';
@@ -7,33 +7,10 @@ import config from './config.js';
 import Header from './Components/Header';
 import TabList from './Components/TabList';
 import Body from './Components/Body';
-const firebase = require('firebase');
 
 //export class App extends Component {
 export function App (props) {
   const [activeTabId, setActiveTabId] = useState(1);
-  const [testdata, setTestdata] = useState("nothing yet");
-  const [shouldRender, setShouldRender] = useState(true);
-  
-  useEffect(() => {
-    if (!firebase.apps.length) {
-     firebase.initializeApp(config)
-    }
-    
-    //get a reference to the database
-    let ref = firebase.database().ref('testdata')
-    //retrieve its data
-    ref.on('value', snapshot => {
-      //this is your call back function
-      //state will be a JSON object after this
-      //set your apps state to contain this data however you like
-      const state = snapshot.val()
-      //since i use react 16, i set my state like this
-      //i have previously declared a state variable like this: const [data, setData] = useState([]) so that I can make the below calls
-      setShouldRender(false)
-      setTestdata(state)
-    })
-  }, [shouldRender])
   
   const changeTab = (tabId) => {
     setActiveTabId(tabId);
@@ -55,7 +32,6 @@ export function App (props) {
           <Header className="header" activeTabId={activeTabId} />
         </div>
         <div className="nav-bar">
-          <h1>{testdata}</h1>
           <img
             className="elem image"
             src={require("./Components/images/lilysprite.png")}
--- src/Components/GuestBook.js
+++ src/Components/GuestBook.js
@@ -1,27 +1,42 @@
 //GuestBook.js
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
+import config from '../config.js';
+const firebase = require('firebase');
 
-export class GuestBook extends Component {
-  render() {
-    return (
-      <div className='GuestBook'>
-        
-        <div class="body-block" id="main-extlinks">
-          <div class="block-content">
-            <div><h2>Social Media</h2></div>
-            <div class="elem"><a href="https://www.facebook.com/lillian.liu.3532">Facebook</a></div> <br/>
-            <div class="elem"><a href="https://www.linkedin.com/in/lillian-liu-657748171/">LinkedIn</a></div> <br/>
-          </div>
-          <div class="block-content">
-            <div><h2>External Links</h2></div>
-            <div class="elem"><a href="https://github.com/liulillian/LillianLiu-CS185">Site GitHub</a></div> <br/>
-            <div class="elem"><a href="https://gauchospace.ucsb.edu/courses/course/view.php?id=76596">CMPSC 185 Course Website</a></div> <br/>
-          </div>
+export function GuestBook (props) {
+  const [testdata, setTestdata] = useState("nothing yet");
+  const [shouldRender, setShouldRender] = useState(true);
+
+  useEffect(() => {
+    if (!firebase.apps.length) {
+     firebase.initializeApp(config)
+    }
+    
+    //get a reference to the database
+    let ref = firebase.database().ref('testdata')
+    //retrieve its data
+    ref.on('value', snapshot => {
+      const state = snapshot.val()
+      setTestdata(state)
+    })
+  }, [shouldRender])
+  
+  return (
+    <div className='GuestBook'>
+      
+      <div class="body-block" id="main-extlinks">
+        <div class="block-content">
+          <div><h2>Social Media</h2></div>
+          <div class="elem"><h3>{testdata}</h3></div> <br/>
         </div>
-          
+        <div class="block-content">
+          <div><h2>External Links</h2></div>
+          <div class="elem"><h3>{testdata}</h3></div> <br/>
+        </div>
       </div>
-    );
-  }
+        
+    </div>
+  );
 }
 
 export default GuestBook;
