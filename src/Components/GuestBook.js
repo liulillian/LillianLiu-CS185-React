e6fe2bd86eef640509bc4d8e7539772f060a3b45
liulillian <l_liu@umail.ucsb.edu>
2020-05-04 19:30:06 -0700

    Array mapping test

--- src/Components/GuestBook.js
+++ src/Components/GuestBook.js
@@ -5,7 +5,10 @@ const firebase = require('firebase');
 
 export function GuestBook (props) {
   const [testdata, setTestdata] = useState("nothing yet");
+  const [firebaseArray, setFirebaseArray] = useState("nothing yet");
   const [shouldRender, setShouldRender] = useState(true);
+  
+  const sampleArray = ['hi','hello','how are you'];
 
   useEffect(() => {
     if (!firebase.apps.length) {
@@ -13,11 +16,16 @@ export function GuestBook (props) {
     }
     
     //get a reference to the database
-    let ref = firebase.database().ref('testdata')
+    let ref1 = firebase.database().ref('testdata')
+    let ref2 = firebase.database().ref('firebaseArray')
     //retrieve its data
-    ref.on('value', snapshot => {
+    ref1.on('value', snapshot => {
       const state = snapshot.val()
       setTestdata(state)
+    })
+    ref2.on('value', snapshot => {
+      const state = snapshot.val()
+      setFirebaseArray(state)
     })
   }, [shouldRender])
   
@@ -31,7 +39,18 @@ export function GuestBook (props) {
         </div>
         <div class="block-content">
           <div><h2>External Links</h2></div>
-          <div class="elem"><h3>{testdata}</h3></div> <br/>
+          <div class="elem">
+            {sampleArray.map((s,index) => (
+              <p>
+                Displaying...<br/>
+                {s}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div class="block-content">
+          <div><h2>Mapping JSON?</h2></div>
+          <div class="elem">Still TODO</div>
         </div>
       </div>
         
